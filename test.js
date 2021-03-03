89a06cb02a5dca07855b8ee6703fca25c487df21
Nathan Allen <nallenanderson@gmail.com>
2021-03-03 11:50:01 -0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,41 @@
+const cart = [
+  {
+    id: 123456,
+    name: 'Puppy Slippers',
+    price: 1099,
+    quantity: 2,
+    shipping: 199,
+  },
+  {
+    id: 234567,
+    name: 'Cat T-Shirt',
+    price: 2249,
+    quantity: 1,
+    shipping: 299
+  },
+  {
+    id: 345678,
+    name: 'Penguin Swim Shorts',
+    price: 699,
+    quantity: 3,
+    shipping: 199,
+  },
+  {
+    id: 123456,
+    name: 'Puppy Slippers',
+    price: 1099,
+    quantity: 2,
+    shipping: 199,
+  },
+  {
+    id: 4567891,
+    name: 'Raccoon Baseball Cap',
+    price: 1699,
+    quantity: 6,
+    shipping: 99,
+  }
+];
+
+// Part One: How much is your total cost assuming price is currently in cents (100 = $1 USD)?
+// Part Two: How can you identify any duplicate in the cart? After identifying, rerun Part One.
+// What if flat-shipping rates were provided if more than 5 items were purchased?
